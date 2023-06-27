dfb9a945048429e46f8062e90f3e93df10cae26c
MATIAS BROGGIA <mbroggia@mail.com>
2023-06-27 12:00:02 -0300

    creacion del about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Swal from "sweetalert2";
+import { Typography, Box } from "@mui/material";
 
 const About = () => {
   useEffect(() => {
@@ -9,8 +10,87 @@ const About = () => {
 
   return (
     <>
-      <Navbar />
-      <h1>HOlaaaaaa</h1>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "16px",
+          height: "100vh",
+        }}
+        style={{
+          backgroundColor: "#282c34",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+          marginLeft: "-8px",
+          marginRight: "-8px",
+          marginBottom: "-8px",
+          overflow: "hidden",
+        }}
+      >
+        <Navbar />
+        <Typography
+          variant="h4"
+          component="h1"
+          gutterBottom
+          style={{ marginTop: "50px" }}
+        >
+          About Us
+        </Typography>
+        <main>
+          <section style={{ marginLeft: "25px" }}>
+            <Typography
+              variant="h5"
+              component="h2"
+              fontWeight="bold"
+              gutterBottom
+            >
+              Our Story
+            </Typography>
+            <Typography variant="body1" component="p">
+              En nuestra página web de rating de películas, nos apasiona el cine
+              y queremos compartir esa pasión con todos los amantes del séptimo
+              arte. Fundada en 2023, nuestra plataforma se ha convertido en un
+              destino confiable para encontrar información valiosa y opiniones
+              sobre las películas más populares. Es una pagina hecha por
+              cinefilos para cinefilos, para poder crear un espacio en comun en
+              donde podamos compartir nuestras opiniones y recomendaciones.
+              Desde nuestros inicios, nos hemos dedicado a ofrecer una
+              experiencia completa para los cinéfilos. Creemos en la importancia
+              de la diversidad cinematográfica y nos esforzamos por incluir
+              películas de diferentes géneros, idiomas y culturas en nuestro
+              catálogo. Valoramos la opinión de nuestra comunidad de usuarios y
+              les brindamos la oportunidad de compartir sus propias
+              calificaciones y reseñas, creando así una plataforma interactiva y
+              colaborativa. Gracias por ser parte de nuestra comunidad de
+              cinéfilos y confiar en nosotros como tu recurso confiable para
+              descubrir y evaluar películas. ¡Esperamos que disfrutes explorando
+              nuestro sitio y encontrando nuevas joyas cinematográficas para
+              disfrutar!
+            </Typography>
+          </section>
+
+          <section style={{ marginTop: "40px", marginLeft: "25px" }}>
+            <Typography
+              variant="h5"
+              component="h2"
+              fontWeight="bold"
+              gutterBottom
+            >
+              Our dev Team
+            </Typography>
+            <ul>
+              <li>Matias Broggia</li>
+              <li>Federico Cortes</li>
+              <li>Adrian Rubado</li>
+              <li>Ursula Cordero</li>
+              <li>Lucio Lama</li>
+              <li>Corina Fontana Medina</li>
+            </ul>
+          </section>
+        </main>
+        <footer>&copy; 2023 Movie Reviews. All rights reserved.</footer>
+      </Box>
     </>
   );
 };
